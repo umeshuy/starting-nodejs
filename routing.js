3b2f43bc08fbf1740ef72390459998d7581d0e1b
umeshuy <110802360+umeshuy@users.noreply.github.com>
2022-12-29 18:21:26 +0530

    printing on screen from file

--- routing.js
+++ routing.js
@@ -4,11 +4,17 @@ const server = http.createServer((req, res) => {
    const url = req.url;
    const method = req.method;
    if (url === '/') {
+      fs.readFile('message.txt',{encoding: "utf-8"}, (err,data)=>{
+         if(err){
+         console.log(err);
+         }
       res.write('<html>');
       res.write('<head><title>Enter Message</title></head>');
+      res.write(`<body>${data}</body>`);
       res.write('<body><form action = "/message" method ="POST"><input type="text" name="message"><button type = "submit">Send</button></form></body>')
       res.write('</html>');
       res.end();
+   })
    }
    if (url === '/message' && method === 'POST') {
       const body = [];
